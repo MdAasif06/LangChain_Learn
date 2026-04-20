91e163230a9ce2a90b15e3bae23db18fcfc8ae68
MdAasif06 <abdurm477@gmail.com>
2026-04-20 12:00:21 +0530

    create chat model using groq api key

---
+++ config/env.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+if (!process.env.GROQ_API_KEY) {
+  throw new Error("Missing API Key");
+}
+
+export const ENV = {
+  GROQ_API_KEY: process.env.GROQ_API_KEY,
+};
